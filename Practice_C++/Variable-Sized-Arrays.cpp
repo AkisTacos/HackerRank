8955cc8f362819463120609aeed8f87241905d9c
AkisTacos <saito.aki93@gmail.com>
2019-04-02 19:14:39 -0700

    fixed

--- Practice_C++/Variable-Sized-Arrays.cpp
+++ Practice_C++/Variable-Sized-Arrays.cpp
@@ -14,13 +14,13 @@
   a[i]. For each query, find and print the value of element j in the array
   at location a[i] on a new line.
 */
+  
 #include <cmath>
 #include <cstdio>
 #include <vector>
 #include <iostream>
 #include <algorithm>
 using namespace std;
-
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
@@ -31,23 +31,24 @@ int main() {
     int numOfRows, numOfQuestion, size, number, i, j;
     vector<vector<int>> rowVector;
     vector<int> colVector;
-    cin >> numOfRows >> numOfQuestion >> size;
-    cout << "numofrow " << numOfRows << " numofQueston " << numOfQuestion << " size " << size;
-    for(i = 0; i << numOfRows; i++){
-       for(j = 0; j << size; j++){
-        cin >> number;
-        colVector.push_back(number);
-      } 
-      rowVector.push_back(colVector);
+    cin >> numOfRows >> numOfQuestion;
+    for(i = 0; i < numOfRows; i++){
+        cin >> size;
+       for(j = 0; j < size; j++){
+            cin >> number;
+            colVector.push_back(number);
+        } 
+        rowVector.push_back(colVector);
+        colVector.clear();
     }
-    
     // Print
     int row, col;
     for(i = 0; i < numOfQuestion; i++){
         cin >> row >> col;
         cout << rowVector[row][col] << endl;
     }
-
     return 0;
 }
+
+
 
