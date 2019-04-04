34a639a369ada1e60639ba4aaaf02388846d5330
AkisTacos <saito.aki93@gmail.com>
2019-04-03 21:50:08 -0700

    Create Strings.cpp

---
+++ Practice_C++/Strings.cpp
@@ -0,0 +1,45 @@
+// Aki Saito
+// HackerRank - Easy
+// Strings
+// April 3, 2019
+// Language: C++
+// 
+/* Description:
+	C++ provides a nice alternative data type to manipulate strings, and the 
+	data type is conveniently called string. 
+	Some of its widely used features are the following:
+		
+		Declaration:
+			string a = "abc";
+		
+		Size:
+			int len = a.size();
+		
+		Concatenate two strings:
+			string a = "abc";
+			string b = "def";
+			string c = a + b; // c = "abcdef".
+		
+		Accessing  element:
+			string s = "abc";
+			char   c0 = s[0];   // c0 = 'a'
+			char   c1 = s[1];   // c1 = 'b'
+			char   c2 = s[2];   // c2 = 'c'
+
+			s[0] = 'z';         // s = "zbc"
+		
+		P.S.: We will use cin/cout to read/write a string.
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	// Complete the program
+    int a, b;
+    
+    
+
+    return 0;
+}
