ba2af715a49e791576585e58c974fe622a664fa7
AkisTacos <ajsaito@ucdavis.edu>
2019-06-12 16:48:59 -0700

    Create Longest_Common_Prefix.cpp

---
+++ Leetcode/Longest_Common_Prefix.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string result = "";
+        
+        // Check for null and vector size 0
+        if(strs.empty() || strs.size() == 0)
+        {
+            return "";
+        }
+        
+        // Loop through first word
+        for(int i = 0; i < strs[0].length(); i++)
+        {
+            char test = strs[0].at(i);
+            // Compare each other string
+            for(int j = 1; j < strs.size(); j++)
+            {
+                if(i == strs[j].length() || strs[j].at(i) != test)
+                {
+                    return strs[0].substr(0, i);
+                }
+            }
+        }
+        
+        return strs[0];
+    }
+};
