a407718efad5c04934ef805c7b1509a642da0e07
AkisTacos <ajsaito@ucdavis.edu>
2019-07-08 15:04:59 -0700

    Create Day-2-Loops.cpp

---
+++ Practice Javascript - 10 Days/Day-2-Loops.cpp
@@ -0,0 +1,65 @@
+/*
+* Aki Saito
+* HackerRank - Easy
+* Javascript - Day 2: Loops
+* July 08, 2019
+* Language: Javascript
+*/
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the vowelsAndConsonants function.
+ * Print your output using 'console.log()'.
+ */
+function vowelsAndConsonants(s) {
+    // Loop so that we check every letter in s
+    var letter, i;
+    var size = s.length;
+
+    // Extract Vowels
+    for (i = 0; i < size; i++) {
+        letter = s.charAt(i);
+        if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u') {
+            console.log(letter);
+        }
+    
+    }
+
+    // Extract Consonants
+    for (i = 0; i < size; i++) {
+        letter = s.charAt(i);
+        if (letter != 'a' && letter != 'e' && letter != 'i' && letter != 'o' && letter != 'u') {
+            console.log(letter);
+        }
+    }
+}
+
+
+function main() {
+    const s = readLine();
+    
+    vowelsAndConsonants(s);
+}
