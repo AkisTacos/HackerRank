ff1d4d68f46b0e0cb63dc7db265c9a6d4376b289
AkisTacos <ajsaito@ucdavis.edu>
2019-08-20 16:23:34 -0700

    Create Day-7-Regular-Expressions-I.js

---
+++ Practice Javascript - 10 Days/Day-7-Regular-Expressions-I.js
@@ -0,0 +1,59 @@
+/*
+* Aki Saito
+* HackerRank - Easy
+* Javascript - Day 7: Regular Expressions I
+* August 20, 2019
+* Language: Javascript
+*/
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts and ends with the same vowel (i.e., {a, e, i, o, u})
+     */
+
+    // Use ^ to assert position at start of string
+    // Capture vowels with ([aeiou])
+    // .* matches any character (except newlines) - between zero and unlimited times
+    //      as many times as possible, giving back as needed
+    // Use \1 to match the same text to most recent matched from group ([aeiou])
+    // Use $ to assert position at end of string
+    let re = /^([aeiou]).*\1$/;
+    
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
+
+
+function main() {
+    const re = regexVar();
+    const s = readLine();
+    
+    console.log(re.test(s));
+}
