dffe52fe62e094e5fcaba35f90ebd4c3d9a941e1
AkisTacos <ajsaito@ucdavis.edu>
2019-08-21 16:47:05 -0700

    Create button.js

---
+++ Practice Javascript - 10 Days/Day-8-Create-A-Button/js/button.js
@@ -0,0 +1,7 @@
+// Javascript file for Day-8-Create-A-Button project
+// 08-21-2019
+
+var btn = document.getElementById("btn");
+btn.addEventListener('click', function() {
+   btn.innerHTML++; 
+});
